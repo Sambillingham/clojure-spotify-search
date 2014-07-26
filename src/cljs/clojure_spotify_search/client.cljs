f67ae2de4d32058e3d126a58102c4d0bfacf15ed
Sam Billingham <sambillie.sb@gmail.com>
2014-07-26 21:35:17 +0100

    basic loop

--- src/cljs/clojure_spotify_search/client.cljs
+++ src/cljs/clojure_spotify_search/client.cljs
@@ -10,15 +10,28 @@
 
 (jq/bind $prevent-default :click (fn [e] (.preventDefault e)))
 
-(defn handler [response]
-  (js/console.log  (str response)))
+;(js/console.log (get {:a 1 :b 2 } :a))
+
+(defn return-tracks [tracks]
+  ((tracks "tracks") "items"))
+
+(defn log-track [response]
+  (js/console.log ((last (return-tracks response)) "name")))
+
+;(defn handler [response]
+;  (js/console.log  ((last ((response "tracks") "items")) "name")))
 
 (defn error-handler [{:keys [status status-text]}]
   (.log js/console (str "something bad happened: " status " " status-text)))
+
+(defn loop-tracks [response]
+  (dotimes [i (count (return-tracks response)) ]
+    (js/console.log (str i ": " (((return-tracks response) i) "name")
+      ))))
 
 (GET "https://api.spotify.com/v1/search"
   {:params {:q "taking back sunday"
             :type "track"
             :limit 5}
-    :handler handler
+    :handler loop-tracks
     :error-handler error-handler})
