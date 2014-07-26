b8a2a99ea5fe974280d685f9ef589f416db74b6e
Sam Billingham <sambillie.sb@gmail.com>
2014-07-26 22:04:01 +0100

    appending

--- src/cljs/clojure_spotify_search/client.cljs
+++ src/cljs/clojure_spotify_search/client.cljs
@@ -12,6 +12,9 @@
 
 ;(js/console.log (get {:a 1 :b 2 } :a))
 
+(defn append-track [track]
+  (jq/append ($ :body) ( str "<div class='track'>Name: " ( track "name" )  " link: " (track "external_url") "</div>" )))
+
 (defn return-tracks [tracks]
   ((tracks "tracks") "items"))
 
@@ -24,10 +27,19 @@
 (defn error-handler [{:keys [status status-text]}]
   (.log js/console (str "something bad happened: " status " " status-text)))
 
+(defn print-track [track]
+  (js/console.log ( str ( track "name") (track "external_url") )))
+
+(append-track {"name" "sam" "external_url" "rodger"})
+
 (defn loop-tracks [response]
   (dotimes [i (count (return-tracks response)) ]
-    (js/console.log (str i ": " (((return-tracks response) i) "name")
-      ))))
+    (append-track ((return-tracks response) i))
+      ))
+
+;(js/console.log (str i ": " ((return-tracks response) i)
+(js/console.log (subvec {"name" "sam" "external_url" "rodger"} "rodger"))
+
 
 (GET "https://api.spotify.com/v1/search"
   {:params {:q "taking back sunday"
